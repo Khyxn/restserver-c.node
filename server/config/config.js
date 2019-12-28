dae401e5365362d593cdac891de7a1c435607285
Nlopezp <oldschool##35>
2019-12-28 11:12:20 -0500

    Actualización

--- server/config/config.js
+++ server/config/config.js
@@ -20,15 +20,9 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'
 }else{
-    // urlDB= 'mongodb+srv://bleda:vYlsoBsUDAZP557r@cluster0-7y2nu.mongodb.net/cafe'
+    
     urlDB= process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
 
-// *****  Conexión local: ******
-// mongodb://localhost:27017/cafe
-
-
-// **** Conexión remota: ****
-// mongodb+srv://bleda:vYlsoBsUDAZP557r@cluster0-7y2nu.mongodb.net/cafe
