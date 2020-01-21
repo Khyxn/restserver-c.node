2ce84e2dd5776d2021a3cbe5b1f44cf5a92a9d58
Nlopezp <oldschool##35>
2020-01-20 21:35:01 -0500

    Google Sing-In

--- server/server.js
+++ server/server.js
@@ -18,14 +18,13 @@ app.use(express.static( path.resolve(__dirname, '../public')));
 // Configuración global de rutas
 app.use(require('./routes/index')); 
 
-
 // let db='mongodb://localhost:27017/cafe';
 let db=process.env.URLDB;
 mongoose.set('useCreateIndex', true);
 mongoose.connect(db,{ useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify:false},(err,res)=>{
    if(err) throw err;
    console.log('Base de datos ONLINE');
-}) 
+})
 app.listen(process.env.PORT,()=>{
     console.log('Escuchando puerto:', process.env.PORT);
 })
